a40e4cbb505f8623e273840f6434cdae909db648
Nicolás A. Rodríguez <nrq5510@gmail.com>
2020-03-16 13:18:52 -0500

    Initial Commit

---
+++ Technical Challenge Kanto/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Technical Challenge Kanto
+//
+//  Created by Nicolás A. Rodríguez on 3/16/20.
+//  Copyright © 2020 Nicolás A. Rodríguez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
